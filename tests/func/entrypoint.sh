cc4af1ac11485f8a5d1d7cc1965fdf1ae454c3c0
Étienne BERSAC <etienne.bersac@dalibo.com>
2019-09-27 10:39:11 +0200

    Check documentation

--- tests/func/entrypoint.sh
+++ tests/func/entrypoint.sh
@@ -33,6 +33,7 @@ mkdir -p $XDG_CACHE_HOME
 pip --disable-pip-version-check install --user poetry
 poetry install
 poetry run flake8 dramatiq_pg/ tests/
+poetry run make -C docs/ check
 poetry run pytest -x tests/unit/
 poetry run tests/pypsql < dramatiq_pg/schema.sql
 poetry run tests/pypsql < tests/func/schema.sql
