12d12cd61cb5a381e9020ced106d00671b5b12e1
Étienne BERSAC <bersace03@cae.li>
2021-04-13 11:44:24 +0200

    Import bdb directly
    To satisfy static analyser like pyright.
    
    Fixes pyright:
    
        /builds/bersace/dramatiq-pg/dramatiq_pg/cli.py
          /builds/bersace/dramatiq-pg/dramatiq_pg/cli.py:30:17 - error: "bdb" is not a known member of module (reportGeneralTypeIssues)

--- dramatiq_pg/cli.py
+++ dramatiq_pg/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import bdb
 import logging
 import os
 import pdb
@@ -27,7 +28,7 @@ def entrypoint():
 
     try:
         exit(main())
-    except (pdb.bdb.BdbQuit, KeyboardInterrupt):
+    except (bdb.BdbQuit, KeyboardInterrupt):
         logger.info("Interrupted.")
     except Exception:
         logger.exception('Unhandled error:')
