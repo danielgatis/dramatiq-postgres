d6278b2c2f66f7046f629311d37768cca1ed6de1
Curtis Maloney <curtis@tinbrain.net>
2024-07-22 17:06:34 +1000

    Parametrize make_pool test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,33 +1,48 @@
-def test_make_pool(mocker):
-    tp = mocker.patch('dramatiq_pg.utils.ThreadedConnectionPool')
-    from dramatiq_pg.utils import make_pool
+import pytest
 
-    pool = make_pool("")
-    assert 16 == pool.minconn
-    tp.reset_mock()
+from dramatiq_pg.utils import make_pool
 
-    pool = make_pool("dbname=toto")
-    call = tp.mock_calls[0]
-    assert "dbname=toto" in call[1][2]
-    assert 16 == pool.minconn
-    tp.reset_mock()
 
-    pool = make_pool("postgresql:///?minconn=4")
-    call = tp.mock_calls[0]
-    assert "minconn" not in call[1][2]
-    assert 4 == pool.minconn
-    tp.reset_mock()
+class TestMakePool:
+    @pytest.fixture
+    def tp(self, mocker):
+        tp = mocker.patch('dramatiq_pg.utils.ThreadedConnectionPool')
+        yield tp
+        tp.reset_mock()
 
-    pool = make_pool("postgresql://host/?minconn=4&maxconn=10")
-    call = tp.mock_calls[0]
-    assert "maxconn" not in call[1][2]
-    assert 4 == pool.minconn
-    tp.reset_mock()
+    def test_empty(self, tp):
+        pool = make_pool("")
 
-    pool = make_pool({"host": "hostname"})
-    call = tp.mock_calls[0]
-    assert "host" in call.kwargs
-    assert call.kwargs["host"] == "hostname"
+        assert 16 == pool.minconn
+
+    def test_keyword(self, tp):
+        pool = make_pool("dbname=toto")
+
+        call = tp.mock_calls[0]
+        assert "dbname=toto" in call[1][2]
+        assert 16 == pool.minconn
+
+    def test_url_param(self, tp):
+        pool = make_pool("postgresql:///?minconn=4")
+
+        call = tp.mock_calls[0]
+        assert "minconn" not in call[1][2]
+        assert 4 == pool.minconn
+
+    def test_min_max(self, tp):
+        pool = make_pool("postgresql://host/?minconn=4&maxconn=10")
+
+        call = tp.mock_calls[0]
+        assert "maxconn" not in call[1][2]
+        assert 4 == pool.minconn
+
+    def test_dict(self, tp):
+        pool = make_pool({"host": "hostname", "minconn": 10})
+
+        call = tp.mock_calls[0]
+        assert "host" in call.kwargs
+        assert call.kwargs["host"] == "hostname"
+        assert 10 == pool.minconn
 
 
 def test_quote_ident():
